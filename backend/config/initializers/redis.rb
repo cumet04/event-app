bf9d8afed8efa15ac553f7ea4481459e32ae01f8
Hikaru Inomoto <medalhkr@gmail.com>
2021-01-17 09:21:04 +0000

    initial clean

---
+++ backend/config/initializers/redis.rb
@@ -0,0 +1,8 @@
+Backend::Application.config.tap do |config|
+  redis_url = "redis://#{ENV.fetch('RAILS_REDIS_HOST')}:#{ENV.fetch('RAILS_REDIS_PORT')}"
+  config.cache_store = :redis_cache_store, {url: "#{redis_url}/0/cache"}
+  config.session_store :redis_store,
+    servers:["#{redis_url}/1/session"],
+    expire_after: 90.minutes,
+    secure: true
+end
